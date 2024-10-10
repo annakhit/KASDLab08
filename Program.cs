a04e06d026711aa051b3cc5c15407845faada6b9
Anna Khit <annakhit02@mail.ru>
2024-10-10 23:23:27 +0300

    Implemented MyStack class

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KASDLab08
 {
@@ -10,6 +6,23 @@ namespace KASDLab08
     {
         static void Main(string[] args)
         {
+            MyStack<int> stack = new MyStack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(4);
+            stack.Push(-1);
+            Console.WriteLine("Вывод стека:");
+            stack.Print();
+
+            Console.WriteLine($"Глубина элемента 4: {stack.Search(4)}");
+
+            Console.WriteLine("Например, удалили элемент с верхушки");
+            stack.Pop();
+            stack.Print();
+            Console.WriteLine("Является ли стек пустым?");
+            Console.WriteLine(stack.Empty());
+            Console.ReadKey();
         }
     }
 }
